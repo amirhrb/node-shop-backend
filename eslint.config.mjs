117cc258facdc90eff623e423d7cce188f6de07c
Amirhrb <amirrabi70@gmail.com>
2025-03-02 13:12:08 +0330

    base project and packages add

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["src/**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
