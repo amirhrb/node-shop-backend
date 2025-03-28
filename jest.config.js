93960184911378c6fc06f08834df843f8b7e94bb
Amirhrb <amirrabi70@gmail.com>
2025-03-28 15:41:44 +0330

    chore(config): update project configuration files

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/*.test.ts"],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  testTimeout: 30000,
+  detectOpenHandles: true,
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+};
